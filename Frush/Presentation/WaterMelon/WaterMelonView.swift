16701e4f4ce857ac8ac3b210844b7962921ca54c
Bibi-urssu <bibi.urssu@gmail.com>
2024-08-16 20:20:20 +0900

    setting: 폴더링

---
+++ Frush/Presentation/WaterMelon/WaterMelonView.swift
@@ -0,0 +1,27 @@
+//
+//  WaterMelonView.swift
+//  Frush
+//
+//  Created by 박지윤 on 8/16/24.
+//
+
+import UIKit
+
+final class WaterMelonView: BaseView {
+
+    // MARK: UI Components
+
+    // MARK: Properties
+
+    // MARK: Configuration
+    override func configureSubviews() {
+        super.configureSubviews()
+    }
+
+    // MARK: Layout
+    override func makeConstraints() {
+        super.makeConstraints()
+    }
+
+    // MARK: Event
+}
---
+++ Frush/Presentation/WaterMelon/WaterMelonViewController.swift
@@ -0,0 +1,26 @@
+//
+//  WaterMelonViewController.swift
+//  Frush
+//
+//  Created by 박지윤 on 8/16/24.
+//
+
+import UIKit
+
+final class WaterMelonViewController: BaseViewController {
+
+    // MARK: UI Components
+
+    // MARK: Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: Configuration
+    override func configureSubviews() {
+    }
+
+    // MARK: Layout
+    override func makeConstraints() {
+    }
+}
