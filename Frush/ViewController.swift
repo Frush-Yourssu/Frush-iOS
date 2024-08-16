7eef98afaca4d4c333562161abb90e0bb1f9ad93
Bibi-urssu <bibi.urssu@gmail.com>
2024-08-16 19:33:59 +0900

    Initial Commit

---
+++ Frush/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Frush
+//
+//  Created by 박지윤 on 8/16/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
