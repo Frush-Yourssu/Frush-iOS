0cdcc714c0fd1bdf42137fea6f670de4c6f3b270
Bibi-urssu <bibi.urssu@gmail.com>
2024-08-16 21:48:41 +0900

    feat: StepView UI 구현

---
+++ Frush/Presentation/FirstStep/FirstStepView.swift
@@ -0,0 +1,49 @@
+//
+//  FirstStepView.swift
+//  Frush
+//
+//  Created by 박지윤 on 8/16/24.
+//
+
+import UIKit
+
+final class FirstStepView: BaseView {
+
+    // MARK: UI Components
+    private let progressBarImageView = UIImageView().then {
+        $0.image = FrushImage.firstProgressBar
+    }
+
+    private let cameraButton = BaseButton().then {
+        $0.setDefaultButton("사진찍기")
+    }
+
+    // MARK: Properties
+    var tapCameraButton: (() -> Void)?
+
+    // MARK: Configuration
+    override func configureSubviews() {
+        addSubview(progressBarImageView)
+        addSubview(cameraButton)
+
+        cameraButton.addTarget(self, action: #selector(handleCameraButtonEvent), for: .touchUpInside)
+    }
+
+    // MARK: Layout
+    override func makeConstraints() {
+        progressBarImageView.snp.makeConstraints {
+            $0.top.equalTo(safeAreaLayoutGuide).inset(12)
+            $0.width.equalToSuperview()
+        }
+
+        cameraButton.snp.makeConstraints {
+            $0.bottom.equalTo(safeAreaLayoutGuide).inset(34)
+            $0.width.equalToSuperview()
+        }
+    }
+
+    // MARK: Event
+    @objc private func handleCameraButtonEvent() {
+        tapCameraButton?()
+    }
+}
---
+++ Frush/Presentation/FirstStep/FirstStepViewController.swift
@@ -0,0 +1,53 @@
+//
+//  FirstStepViewController.swift
+//  Frush
+//
+//  Created by 박지윤 on 8/16/24.
+//
+
+import UIKit
+
+final class FirstStepViewController: BaseViewController {
+
+    // MARK: UI Components
+    private let backButton = BaseButton().then {
+        $0.setImage(FrushButton.backButton, for: .normal)
+    }
+
+    private let firstStepView = FirstStepView()
+
+    // MARK: Environment
+    private let router = BaseRouter()
+
+    // MARK: Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setNavigationItem()
+
+        router.viewController = self
+    }
+
+    // MARK: Configuration
+    override func configureSubviews() {
+        view.addSubview(firstStepView)
+
+        backButton.tap = { [weak self] in
+            guard let self else { return }
+            router.dismissViewController()
+        }
+    }
+
+    // MARK: Layout
+    override func makeConstraints() {
+        firstStepView.snp.makeConstraints {
+            $0.verticalEdges.equalToSuperview()
+            $0.horizontalEdges.equalToSuperview().inset(16)
+        }
+    }
+
+    // MARK: Navigation Item
+    private func setNavigationItem() {
+        navigationItem.titleView = UIImageView(image: FrushImage.frushLogo)
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: backButton)
+    }
+}
