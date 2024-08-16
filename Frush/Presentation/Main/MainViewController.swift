202a1a363ca5a922db02f79dc3e4321f58472342
Bibi-urssu <bibi.urssu@gmail.com>
2024-08-16 20:46:40 +0900

    feat: MainView 화면 이동 연결

--- Frush/Presentation/Main/MainViewController.swift
+++ Frush/Presentation/Main/MainViewController.swift
@@ -10,17 +10,55 @@ import UIKit
 final class MainViewController: BaseViewController {
 
     // MARK: UI Components
+    private let mainView = MainView()
+
+    // MARK: Environment
+    private let router = BaseRouter()
 
     // MARK: Life Cycle
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        router.viewController = self
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        setNavigationItem()
     }
 
     // MARK: Configuration
     override func configureSubviews() {
+        view.backgroundColor = .white
+
+        view.addSubview(mainView)
+
+        mainView.tapWaterMelonButton = { [weak self] in
+            guard let self else { return }
+            router.presentWaterMelonViewController()
+        }
+
+        mainView.tapPeachButton = { [weak self] in
+            guard let self else { return }
+            router.presentPeachViewController()
+        }
+
+        mainView.tapKoreanMelonButton = { [weak self] in
+            guard let self else { return }
+            router.presentKoreanMelonViewController()
+        }
     }
 
     // MARK: Layout
     override func makeConstraints() {
+        mainView.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview()
+            $0.horizontalEdges.equalToSuperview().inset(20)
+        }
+    }
+
+    // MARK: Navigation Item
+    private func setNavigationItem() {
+        navigationItem.title = "프루티"
     }
 }
--- Frush/Presentation/WaterMelon/WaterMelonViewController.swift
+++ Frush/Presentation/WaterMelon/WaterMelonViewController.swift
@@ -14,6 +14,7 @@ final class WaterMelonViewController: BaseViewController {
     // MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        setNavigationItem()
     }
 
     // MARK: Configuration
@@ -22,5 +23,10 @@ final class WaterMelonViewController: BaseViewController {
 
     // MARK: Layout
     override func makeConstraints() {
+    }
+
+    // MARK: Navigation Item
+    private func setNavigationItem() {
+        navigationItem.title = "수박"
     }
 }
