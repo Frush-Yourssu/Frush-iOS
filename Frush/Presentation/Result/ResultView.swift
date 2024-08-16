d76c4bee18cd5887a3e48289e004fe36ae1b9f7e
Bibi-urssu <bibi.urssu@gmail.com>
2024-08-16 22:11:11 +0900

    feat: LoadingView & ResultView UI 구현

--- Frush/Presentation/Result/ResultView.swift
+++ Frush/Presentation/Result/ResultView.swift
@@ -10,17 +10,60 @@ import UIKit
 final class ResultView: BaseView {
 
     // MARK: UI Components
+    private let resultLabel = UILabel().then {
+        $0.text = "내가 고른 수박은?"
+        $0.textColor = .black
+        $0.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+    }
+
+    private let waterMelonImageView = UIImageView().then {
+        $0.backgroundColor = .lightGray
+    }
+
+    private let waterMelonLabel = UILabel().then {
+        $0.text = "맛없는 수박"
+        $0.textColor = .black
+        $0.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+    }
+
+    private let descriptLabel = UILabel().then {
+        $0.text = "달콤함을 기대하지 마세요.. 씁쓸할 거에요."
+        $0.textColor = .black
+        $0.font = UIFont.systemFont(ofSize: 16, weight: .medium)
+    }
 
     // MARK: Properties
 
     // MARK: Configuration
     override func configureSubviews() {
-        super.configureSubviews()
+        addSubview(resultLabel)
+        addSubview(waterMelonImageView)
+        addSubview(waterMelonLabel)
+        addSubview(descriptLabel)
     }
 
     // MARK: Layout
     override func makeConstraints() {
-        super.makeConstraints()
+        resultLabel.snp.makeConstraints {
+            $0.top.equalTo(safeAreaLayoutGuide).inset(48)
+            $0.centerX.equalToSuperview()
+        }
+
+        waterMelonImageView.snp.makeConstraints {
+            $0.top.equalTo(resultLabel.snp.bottom).offset(20)
+            $0.centerX.equalToSuperview()
+            $0.width.height.equalTo(180)
+        }
+
+        waterMelonLabel.snp.makeConstraints {
+            $0.top.equalTo(waterMelonImageView.snp.bottom).offset(20)
+            $0.centerX.equalToSuperview()
+        }
+
+        descriptLabel.snp.makeConstraints {
+            $0.top.equalTo(waterMelonLabel.snp.bottom).offset(12)
+            $0.centerX.equalToSuperview()
+        }
     }
 
     // MARK: Event
--- Frush/Presentation/Result/ResultViewController.swift
+++ Frush/Presentation/Result/ResultViewController.swift
@@ -10,17 +10,30 @@ import UIKit
 final class ResultViewController: BaseViewController {
 
     // MARK: UI Components
+    private let resultView = ResultView()
 
     // MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        setNavigationItem()
     }
 
     // MARK: Configuration
     override func configureSubviews() {
+        view.addSubview(resultView)
     }
 
     // MARK: Layout
     override func makeConstraints() {
+        resultView.snp.makeConstraints {
+            $0.verticalEdges.equalToSuperview()
+            $0.horizontalEdges.equalToSuperview().inset(16)
+        }
+    }
+
+    // MARK: Navigation Item
+    private func setNavigationItem() {
+        navigationItem.titleView = UIImageView(image: FrushImage.frushLogo)
+        navigationItem.hidesBackButton = true
     }
 }
