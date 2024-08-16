e5def8b808fb0f01d4de582242b584034ef3262b
Bibi-urssu <bibi.urssu@gmail.com>
2024-08-16 20:24:15 +0900

    setting: 폴더링 - 2

---
+++ Frush/Presentation/Result/ResultView.swift
@@ -0,0 +1,27 @@
+//
+//  ResultView.swift
+//  Frush
+//
+//  Created by 박지윤 on 8/16/24.
+//
+
+import UIKit
+
+final class ResultView: BaseView {
+
+    // MARK: UI Components
+
+    // MARK: Properties
+
+    // MARK: Configuration
+    override func configureSubviews() {
+        super.configureSubviews()
+    }
+
+    // MARK: Layout
+    override func makeConstraints() {
+        super.makeConstraints()
+    }
+
+    // MARK: Event
+}
