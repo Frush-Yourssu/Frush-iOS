949c88be9e7a894906f9ec1cee643eac539ad0b6
Bibi-urssu <bibi.urssu@gmail.com>
2024-08-17 02:05:48 +0900

    add: UILabel+Extension

---
+++ Frush/Extension/UILabel+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  UILabel+Extension.swift
+//  Frush
+//
+//  Created by 박지윤 on 8/17/24.
+//
+
+import UIKit
+
+extension UILabel {
+    func setDefaultLabel(_ inputText: String) {
+        text = inputText
+        textColor = .black
+        font = UIFont(name: "GmarketSansTTFBold", size: 20)
+    }
+
+    func setLabel() {
+        
+    }
+}
