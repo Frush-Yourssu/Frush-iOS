cf66d26c4037c70f1a9d8e8608e17bd7a6b7c54f
Bibi-urssu <bibi.urssu@gmail.com>
2024-08-17 06:37:18 +0900

    add: dummyData 추가

--- Frush/Networking/DummyData.swift
+++ Frush/Networking/DummyData.swift
@@ -19,3 +19,32 @@ extension WaterMelonData {
         WaterMelonData(category: "줄무늬", similarity: 70)
     ]
 }
+
+struct FruitData {
+    let fruit: String
+    let fruitPart: String
+}
+
+extension FruitData {
+    static let waterMelonDataList = [
+        FruitData(fruit: "WATER_MELON", fruitPart: "WATER_MELON_CIRCULAR"),
+        FruitData(fruit: "WATER_MELON", fruitPart: "WATER_MELON_STRIPES"),
+        FruitData(fruit: "WATER_MELON", fruitPart: "WATER_MELON_NAVEL")
+    ]
+
+    static let koreanMelonDataList = [
+        FruitData(fruit: "ORIENTAL_MELON", fruitPart: "ORIENTAL_MELON_NAVEL"),
+        FruitData(fruit: "ORIENTAL_MELON", fruitPart: "ORIENTAL_MELON_OVAL"),
+        FruitData(fruit: "ORIENTAL_MELON", fruitPart: "ORIENTAL_MELON_INJURY")
+    ]
+
+    static let peachDataList = [
+        FruitData(fruit: "PEACH", fruitPart: "PEACH_RED"),
+        FruitData(fruit: "PEACH", fruitPart: "PEACH_LINE"),
+        FruitData(fruit: "PEACH", fruitPart: "PEACH_INJURY")
+    ]
+}
+
+struct FrushRealData {
+    static var frushRealDataList: [FrushResponse] = []
+}
