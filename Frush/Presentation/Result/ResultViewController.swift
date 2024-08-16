0487f54191dc5e57ccd42573687d2049e8e9e6b5
Bibi-urssu <bibi.urssu@gmail.com>
2024-08-17 01:10:12 +0900

    feat: ResultViewController dismissButton 추가

--- Frush/Presentation/Result/ResultViewController.swift
+++ Frush/Presentation/Result/ResultViewController.swift
@@ -12,15 +12,29 @@ final class ResultViewController: BaseViewController {
     // MARK: UI Components
     private let resultView = ResultView()
 
+    private let dismissButton = BaseButton().then {
+        $0.setImage(FrushButton.dismissButton, for: .normal)
+    }
+
+    // MARK: Environment
+    private let router = BaseRouter()
+
     // MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setNavigationItem()
+
+        router.viewController = self
     }
 
     // MARK: Configuration
     override func configureSubviews() {
         view.addSubview(resultView)
+
+        dismissButton.tap = { [weak self] in
+            guard let self else { return }
+            router.dismissViewController()
+        }
     }
 
     // MARK: Layout
@@ -35,5 +49,6 @@ final class ResultViewController: BaseViewController {
     private func setNavigationItem() {
         navigationItem.titleView = UIImageView(image: FrushImage.frushLogo)
         navigationItem.hidesBackButton = true
+        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: dismissButton)
     }
 }
