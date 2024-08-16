948fd4caacedebe48030c05affaebef1e0a79496
Bibi-urssu <bibi.urssu@gmail.com>
2024-08-16 20:24:15 +0900

    setting: 폴더링 - 2

---
+++ Frush/Presentation/Loading/LoadingViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LoadingViewController.swift
+//  Frush
+//
+//  Created by 박지윤 on 8/16/24.
+//
+
+import UIKit
+
+final class LoadingViewController: BaseViewController {
+
+    // MARK: UI Components
+
+    // MARK: Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: Configuration
+    override func configureSubviews() {
+    }
+
+    // MARK: Layout
+    override func makeConstraints() {
+    }
+}
