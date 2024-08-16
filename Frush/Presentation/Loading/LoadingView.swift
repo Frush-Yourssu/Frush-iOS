61fddb707ef179d4ee34577d96aeec381d4cfe05
Bibi-urssu <bibi.urssu@gmail.com>
2024-08-16 22:11:11 +0900

    feat: LoadingView & ResultView UI 구현

--- Frush/Presentation/Loading/LoadingView.swift
+++ Frush/Presentation/Loading/LoadingView.swift
@@ -10,17 +10,25 @@ import UIKit
 final class LoadingView: BaseView {
 
     // MARK: UI Components
+    private let loadingLabel = UILabel().then {
+        $0.text = "결과를 분석 중입니다.."
+        $0.textColor = .black
+        $0.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+    }
 
     // MARK: Properties
 
     // MARK: Configuration
     override func configureSubviews() {
-        super.configureSubviews()
+        addSubview(loadingLabel)
     }
 
     // MARK: Layout
     override func makeConstraints() {
-        super.makeConstraints()
+        loadingLabel.snp.makeConstraints {
+            $0.top.equalTo(safeAreaLayoutGuide).inset(56)
+            $0.centerX.equalToSuperview()
+        }
     }
 
     // MARK: Event
--- Frush/Presentation/Loading/LoadingViewController.swift
+++ Frush/Presentation/Loading/LoadingViewController.swift
@@ -10,17 +10,30 @@ import UIKit
 final class LoadingViewController: BaseViewController {
 
     // MARK: UI Components
+    private let loadingView = LoadingView()
 
     // MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        setNavigationItem()
     }
 
     // MARK: Configuration
     override func configureSubviews() {
+        view.addSubview(loadingView)
     }
 
     // MARK: Layout
     override func makeConstraints() {
+        loadingView.snp.makeConstraints {
+            $0.verticalEdges.equalToSuperview()
+            $0.horizontalEdges.equalToSuperview().inset(16)
+        }
+    }
+
+    // MARK: Navigation Item
+    private func setNavigationItem() {
+        navigationItem.titleView = UIImageView(image: FrushImage.frushLogo)
+        navigationItem.hidesBackButton = true
     }
 }
